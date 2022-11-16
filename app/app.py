bbe334d7ead032f031664a6130bf7b7912e40c5d
Kevin Kohler <kohlerkevin@outlook.de>
2022-11-16 17:02:04 +0100

    Add basic disclaimer

--- app/app.py
+++ app/app.py
@@ -106,3 +106,5 @@ with tab3:
 
         print (f'Shape of Train Data : {X_train.shape}')
         print (f'Shape of Test Data : {X_test.shape}')
+
+st.caption('dora (Diabetes Occurrence Risk Assessment) is a Streamlit app which uses XGBoost on the Pima Indians Dataset in order to predict user\'s risks for Diabetes Type II. This is a test app that cannot replace professional medical advice. In case of health issues, seek help from experts. No liability is assumed for the outcomes of this app.')
